30e24f77c24787878a146e2f049309dffc0d7582
yzx012824 <2521771982@qq.com>
2020-08-19 19:52:49 +0800

    添加hello方法

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.jk.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class UserController {
+
+    @RequestMapping("/hello")
+    public void hello(){
+
+    }
+
 }
