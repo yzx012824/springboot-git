86e2e0cb1a95a588776e0fbdcc35443b4b83353e
yzx012824 <2521771982@qq.com>
2020-08-19 21:20:54 +0800

    1231

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.jk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+
+    @RequestMapping("/hi")
+    public String hi(){
+        return "123";
+    }
 }
